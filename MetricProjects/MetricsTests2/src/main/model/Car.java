20d5e223573127e3ec529c9a0ea98be4923297e9
Thanus <tthanusijan@gmail.com>
2016-11-10 10:02:42 +0100

    add car class in a nested folder

---
+++ MetricProjects/MetricsTests2/src/main/model/Car.java
@@ -0,0 +1,21 @@
+package main.model;
+
+public class Car {
+	
+	private String name;
+	private int wheels;
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getWheels() {
+		return wheels;
+	}
+	public void setWheels(int wheels) {
+		this.wheels = wheels;
+	}
+	
+}
