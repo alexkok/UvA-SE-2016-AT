c7bc7311171ac2a91b8a55e35e3b173d590d967b
Alex Kok <alexkok08@gmail.com>
2016-12-18 16:28:56 +0100

    Adding the clone details based on the actual values now

--- Series2/src/result/js/details.js
+++ Series2/src/result/js/details.js
@@ -8,7 +8,6 @@ $.getJSON("data/details.json", function(json) {
 
     // Load analysis details immediately
     if (jsonData) {
-        console.log(jsonData);
         document.getElementById("analysesDetailsStartTime").innerHTML = jsonData.time_start;
         document.getElementById("analysesDetailsEndTime").innerHTML = jsonData.time_end;
         document.getElementById("analysesDetailsDuration").innerHTML = jsonData.total_duration;
@@ -21,17 +20,13 @@ $.getJSON("data/details.json", function(json) {
 loadCloneDetails = function (path) {
     if (path.endsWith("java")) {
         var cloneDetails = getDetailsForPath(path);
-        console.log(cloneDetails);
         if (cloneDetails) {
             showNodeDetails(path, cloneDetails);
-            console.log("showing node details");
         } else {
             showNoNodeSelected();
-            console.log("showing no node 1");
         }
     } else {
         showNoNodeSelected();
-        console.log("showing no node 2");
     }
 };
 
@@ -46,12 +41,53 @@ function getDetailsForPath(path) {
 
 function showNoNodeSelected() {
     document.getElementById("selectedCloneText").innerHTML = "No node selected";
-    $("#selectedCloneDetails").hide();
+    $(".selectedCloneDetails").hide();
 }
 
 function showNodeDetails(path, cloneDetails) {
     document.getElementById("selectedCloneText").innerHTML = path;
     document.getElementById("selectedCloneClonesFound").innerHTML = cloneDetails.clones.length;
     document.getElementById("selectedCloneDuplicatedLOC").innerHTML = cloneDetails.loc_duplicate;
-    $("#selectedCloneDetails").show();
+    $(".selectedCloneDetails").show();
+
+    var cloneDetailPanel = document.getElementById("selectedCloneDetailsPanel");
+    // Clear old childs
+    while (cloneDetailPanel.firstChild) {
+        cloneDetailPanel.removeChild(cloneDetailPanel.firstChild);
+    }
+    // For each clone: create html object and add it to the body
+    for (var i = 0; i < cloneDetails.clones.length; i++) {
+        var clonePanel = createHTMLObjectForClone(i, cloneDetails.clones[i]);
+        cloneDetailPanel.appendChild(clonePanel);
+    }
+
+}
+function createHTMLObjectForClone(id, clone) {
+    var panel = document.createElement("div");
+    panel.className = "panel panel-default";
+    // The heading
+    var panelHeading = document.createElement("div");
+    panelHeading.className = "panel-heading";
+    var panelHeadingCollapse = document.createElement("a");
+    panelHeadingCollapse.setAttribute("data-toggle", "collapse");
+    panelHeadingCollapse.setAttribute("href", "#collapse" + id);
+    var panelHeadingTitle = document.createElement("h4");
+    panelHeadingTitle.className = "panel-title";
+    // panelHeadingTitle.innerHTML = "xx lines (xx - xx)";
+    panelHeadingTitle.innerHTML = clone.total_lines + " lines (" + clone.line_start + " - " + clone.line_end + ")";
+    panelHeadingCollapse.appendChild(panelHeadingTitle);
+    panelHeading.appendChild(panelHeadingCollapse);
+    // The body
+    var collapsingBody = document.createElement("div");
+    collapsingBody.id = "collapse" + id;
+    collapsingBody.className = "panel-collapse collapse";
+    var panelBody= document.createElement("div");
+    panelBody.className = "panel-body";
+    panelBody.innerHTML = clone.content;
+    collapsingBody.appendChild(panelBody);
+
+    // Adding heading and body to the panel
+    panel.appendChild(panelHeading);
+    panel.appendChild(collapsingBody);
+    return panel;
 }
